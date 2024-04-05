406f5c44d95e063d560c5521c427a6c5a2709f30
jamisaiteja <jamisaiteja666@gmail.com>
2024-04-05 12:49:36 +0530

    changes

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 let avaliableKeywords = [
-    "HTML",
-    'css',
-    'Easy Tutorials',
-    'Web design tutorials',
-    'JavaScript',
-    'Where to learn coding online',
-    'Where to learn web design',
-    'How to create a website'
+  "HTML",
+  "css",
+  "Easy Tutorials",
+  "Web design tutorials",
+  "JavaScript",
+  "Where to learn coding online",
+  "Where to learn web design",
+  "How to create a website",
 ];
-
 
 const resultBox = document.querySelector(".result-box");
 const inputBox = document.getElementById("input-box");
 
-inputBox.onkeyup = ()=>{
-    let result =[];
-    let input = inputBox.value;
-    if(input.length){
-        result = avaliableKeywords.filter((keyword)=>{
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-        console.log(result);
-    }
-    display(result);
+inputBox.onkeyup = () => {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = avaliableKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
 
-    if(!result.length){
-        resultBox.innerHTML="";a  
-    }
-}
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
+};
 
 function display(result) {
-    const content = result.map((list)=>{
-        return "<li onclick = selectInput(this)>" + list + "</li>";
-    });
+  const content = result.map((list) => {
+    return "<li onclick = selectInput(this)>" + list + "</li>";
+  });
 
-    resultBox.innerHTML = "<ul>"+ content.join(" ") + "</ul>";
+  resultBox.innerHTML = "<ul>" + content.join(" ") + "</ul>";
 }
 
-function selectInput(list){
-    inputBox.value = list.innerHTML;
-    resultBox.innerHTML = " ";
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = " ";
 }
